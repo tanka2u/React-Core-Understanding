28b8cedb1b7a8534f44a4bc87ae707943b61cbf9
tanka2u <ptanka123@gmail.com>
2024-08-26 15:10:18 +0200

    Before using backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import MainHeader from "./components/MainHeader";
+import PostList from "./components/PostList";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+  function showModalHandler() {
+    setModalIsVisible(true);
+  }
 
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MainHeader onCreatePost={showModalHandler} />
+      <main>
+        <PostList isPosting={modalIsVisible} onStopPosting={hideModalHandler} />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/PostList.jsx
@@ -0,0 +1,37 @@
+import classes from "./PostList.module.css";
+import Post from "./Post";
+import NewPost from "./NewPost";
+import Modal from "./Modal";
+import { useState } from "react";
+
+function PostList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  }
+  return (
+    <>
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
+        </Modal>
+      )}
+      {posts.length > 0 && (
+        <ul className={classes.postlists}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no Posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default PostList;
